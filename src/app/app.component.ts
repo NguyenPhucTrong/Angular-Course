8f8e3cc283d51a9b643e714ffb53294409b7b166
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-09-28 21:41:57 +0700

    course 4 (ngFor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 // import { HeaderComponent } from "./header/header.component";
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopHeaderComponent, FooterComponent],
+  imports: [RouterOutlet, TopHeaderComponent, FooterComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,14 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent {
+  menu: string[] = ["menu", " contact", " about"];
+
+}
